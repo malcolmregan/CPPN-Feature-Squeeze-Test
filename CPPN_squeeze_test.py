e98d0567aea6c98f121423477104210a82bdd0fc
malcolmregan <mjregan1@asu.edu>
2017-09-23 16:06:14 -0500

    added viz, cleaned up

--- CPPN_squeeze_test.py
+++ CPPN_squeeze_test.py
@@ -6,6 +6,7 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, Flatten
 import numpy as np
+import matplotlib.pyplot as plt
 
 if keras.backend.image_dim_ordering() != 'tf':
     keras.backend.set_image_dim_ordering('tf')
@@ -48,6 +49,7 @@ print('')
 print('Testing model on CPPN examples...')
 correct_count=0
 wrong_count=0
+bad_example_marker=789
 for i in range(number_of_examples):
     example = CPPN_examples[i:i+1,:,:,:,0]
     target = np.where(CPPN_examples[i,0,0,0,:]==1)[0][0]
@@ -55,9 +57,9 @@ for i in range(number_of_examples):
     if pred[target]>=0.85:
         correct_count = correct_count + 1
     if pred[target]<.85:
-        CPPN_examples[i,0,0,:,0]=789
+        CPPN_examples[i,0,0,:,0]=bad_example_marker
         wrong_count=wrong_count+1
-trick_rate = float(correct_count)*100/float(number_of_examples)
+trick_rate = float(correct_count)*100/number_of_examples
 number_of_examples=number_of_examples-wrong_count
 
 print('')
@@ -72,7 +74,7 @@ no_classification = 0
 noise_classification = 0
 for i in range(np.shape(CPPN_examples)[0]):
     example = CPPN_examples[i:i+1,:,:,:,0]
-    if example[0,0,0,:]!=789:
+    if example[0,0,0,:]!=bad_example_marker:
         target = np.where(CPPN_examples[i,0,0,0,:]==1)[0][0]
         example = binary_filter_np(example)
         pred = predictions.eval(session=sess, feed_dict={x: example})[0]
@@ -85,26 +87,27 @@ for i in range(np.shape(CPPN_examples)[0]):
                     noise_classification=noise_classification+1
                 if most_likely_class!=10:
                     new_classification_not_noise=new_classification_not_noise+1
-        if pred[most_likely_class]<=0.85:
+        if pred[most_likely_class]<0.85:
             no_classification=no_classification+1
 
 print('')
-print('Same Classification: {:.2f}%').format(same_classification*100/number_of_examples)
-print('New Classification Other Than Noise: {:.2f}%').format(new_classification_not_noise*100/number_of_examples)
-print('No Classification: {:.2f}%').format(no_classification*100/number_of_examples)
-print('Noise Classification: {:.2f}%').format(noise_classification*100/number_of_examples)
+print('Same Classification: {:.2f}%').format(float(same_classification)*100/number_of_examples)
+print('New Classification Other Than Noise: {:.2f}%').format(float(new_classification_not_noise)*100/number_of_examples)
+print('No Classification: {:.2f}%').format(float(no_classification)*100/number_of_examples)
+print('Noise Classification: {:.2f}%').format(float(noise_classification)*100/number_of_examples)
 
 print('')
 print("Testing model on median filtered CPPN examples...")
+median_param = 50
 same_classification = 0
 new_classification_not_noise = 0
 no_classification = 0
 noise_classification = 0
 for i in range(np.shape(CPPN_examples)[0]):
     example = CPPN_examples[i:i+1,:,:,:,0]
-    if example[0,0,0,:]!=789:
+    if example[0,0,0,:]!=bad_example_marker:
         target = np.where(CPPN_examples[i,0,0,0,:]==1)[0][0]
-        example = median_filter_np(example,5) # second arguement is width of sliding window
+        example = median_filter_np(example,median_param) # second arguement is width of sliding window
         pred = predictions.eval(session=sess, feed_dict={x: example})[0]
         most_likely_class = np.argmax(pred)
         if pred[most_likely_class]>=0.85:
@@ -115,26 +118,27 @@ for i in range(np.shape(CPPN_examples)[0]):
                     noise_classification=noise_classification+1
                 if most_likely_class!=10:
                     new_classification_not_noise=new_classification_not_noise+1
-        if pred[most_likely_class]<=0.85:
+        if pred[most_likely_class]<0.85:
             no_classification=no_classification+1
 
 print('')
-print('Same Classification: {:.2f}%').format(same_classification*100/number_of_examples)
-print('New Classification Other Than Noise: {:.2f}%').format(new_classification_not_noise*100/number_of_examples)
-print('No Classification: {:.2f}%').format(no_classification*100/number_of_examples)
-print('Noise Classification: {:.2f}%').format(noise_classification*100/number_of_examples)
+print('Same Classification: {:.2f}%').format(float(same_classification)*100/number_of_examples)
+print('New Classification Other Than Noise: {:.2f}%').format(float(new_classification_not_noise)*100/number_of_examples)
+print('No Classification: {:.2f}%').format(float(no_classification)*100/number_of_examples)
+print('Noise Classification: {:.2f}%').format(float(noise_classification)*100/number_of_examples)
 
 print('')
 print("Testing model on reduced precision CPPN examples...")
+precision_param=100
 same_classification = 0
 new_classification_not_noise = 0
 no_classification = 0
 noise_classification = 0
 for i in range(np.shape(CPPN_examples)[0]):
     example = CPPN_examples[i:i+1,:,:,:,0]
-    if example[0,0,0,:]!=789:
+    if example[0,0,0,:]!=bad_example_marker:
         target = np.where(CPPN_examples[i,0,0,0,:]==1)[0][0]
-        example = reduce_precision_np(example,5)
+        example = reduce_precision_np(example,precision_param)
         pred = predictions.eval(session=sess, feed_dict={x: example})[0]
         most_likely_class = np.argmax(pred)
         if pred[most_likely_class]>=0.85:
@@ -145,12 +149,61 @@ for i in range(np.shape(CPPN_examples)[0]):
                     noise_classification=noise_classification+1
                 if most_likely_class!=10:
                     new_classification_not_noise=new_classification_not_noise+1
-        if pred[most_likely_class]<=0.85:
+        if pred[most_likely_class]<0.85:
             no_classification=no_classification+1
 
 print('')
-print('Same Classification: {:.2f}%').format(same_classification*100/number_of_examples)
-print('New Classification Other Than Noise: {:.2f}%').format(new_classification_not_noise*100/number_of_examples)
-print('No Classification: {:.2f}%').format(no_classification*100/number_of_examples)
-print('Noise Classification: {:.2f}%').format(noise_classification*100/number_of_examples)
+print('Same Classification: {:.2f}%').format(float(same_classification)*100/number_of_examples)
+print('New Classification Other Than Noise: {:.2f}%').format(float(new_classification_not_noise)*100/number_of_examples)
+print('No Classification: {:.2f}%').format(float(no_classification)*100/number_of_examples)
+print('Noise Classification: {:.2f}%').format(float(noise_classification)*100/number_of_examples)
 
+#Visualize
+print('')
+print('Visualizing examples...')
+print('')
+
+fig = plt.figure()
+random_examples = np.random.random_integers(number_of_examples, size=(1,5))[0]
+ax = [0]*4*np.shape(random_examples)[0]
+for num, i in enumerate(random_examples):
+    example = CPPN_examples[i:i+1,:,:,:,0]
+    while example[0,0,0,:]==bad_example_marker or np.count_nonzero(random_examples==random_examples[num])!=1:
+            random_examples[num]=np.random.random_integers(number_of_examples)
+            example = CPPN_examples[random_examples[num]:random_examples[num]+1,:,:,:,0]
+    binary = binary_filter_np(example)
+    median = median_filter_np(example,median_param)
+    reduced_precision = reduce_precision_np(example,precision_param)
+    ax[num*4] = fig.add_subplot(np.shape(random_examples)[0], 4, num*4+1, aspect='equal')
+    ax[num*4].imshow(example[0,:,:,0], cmap='gray_r', interpolation='nearest')
+    ax[num*4].axes.get_xaxis().set_visible(False)
+    ax[num*4].axes.get_yaxis().set_visible(False)
+
+    ax[num*4+1] = fig.add_subplot(np.shape(random_examples)[0], 4, num*4+2, aspect='equal')
+    ax[num*4+1].imshow(binary[0,:,:,0], cmap='gray_r', interpolation='nearest')
+    ax[num*4+1].axes.get_xaxis().set_visible(False)
+    ax[num*4+1].axes.get_yaxis().set_visible(False)
+
+    ax[num*4+2] = fig.add_subplot(np.shape(random_examples)[0], 4, num*4+3, aspect='equal')
+    ax[num*4+2].imshow(median[0,:,:,0], cmap='gray_r', interpolation='nearest')
+    ax[num*4+2].axes.get_xaxis().set_visible(False)
+    ax[num*4+2].axes.get_yaxis().set_visible(False)
+
+    ax[num*4+3] = fig.add_subplot(np.shape(random_examples)[0], 4, num*4+4, aspect='equal')
+    ax[num*4+3].imshow(reduced_precision[0,:,:,0], cmap='gray_r', interpolation='nearest')
+    ax[num*4+3].axes.get_xaxis().set_visible(False)
+    ax[num*4+3].axes.get_yaxis().set_visible(False)
+
+    if num==0:
+       ax[num*4].set_title('CPPN')
+       ax[num*4+1].set_title('Binary')
+       ax[num*4+2].set_title('Median')
+       ax[num*4+3].set_title('Reduced Precision')   
+
+manager = plt.get_current_fig_manager()
+manager.window.showMaximized()
+
+fig.tight_layout()
+fig.subplots_adjust(top=0.93)
+
+plt.show()
